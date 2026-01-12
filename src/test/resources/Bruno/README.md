a1b5fd40bc2c9f1698cee3b2afeeecfde3f2d8f6
Ellie Newell <ellie.newell@digital.hmrc.gov.uk>
2026-01-12 11:15:27 +0000

    CIR-2603b (#4)
    * CIR-2603b - Added tests for check/insights and Bruno config
    
    * CIR-2603b - Changed bruno collection name
    
    * CIR-2603b - Amended payload value and added counts endpoint

---
+++ src/test/resources/Bruno/README.md
@@ -0,0 +1,18 @@
+
+# Phone Number Insights API Bruno Collection
+
+These are the API calls for manual testing in Bruno.
+
+## How to import/open locally
+
+1. Open Bruno
+2. Click the + in the Collections tab
+3. Select Open collection
+4. Navigate to /src/test/resources/Bruno and click the Bruno folder once
+5. Click Open
+
+### How to update collection
+
+Bruno is configured to your local acceptance test repository. 
+Every time you make a change it will amend the collection automatically and update the changes in the repository.
+Commit changes if you want to keep them, otherwise remove the Bruno changes before committing other changes.
--- src/test/scala/uk/gov/hmrc/api/specs/PhoneNumberInsightsSpec.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/PhoneNumberInsightsSpec.scala
@@ -17,38 +17,86 @@
 package uk.gov.hmrc.api.specs
 
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
-import uk.gov.hmrc.api.helpers.PhoneNumberInsightsHelpers
 
-class PhoneNumberInsightsSpec extends PhoneNumberInsightsHelpers with BeforeAndAfterEach with BeforeAndAfterAll {
+class PhoneNumberInsightsSpec extends BaseSpec with BeforeAndAfterEach with BeforeAndAfterAll {
+
+  val watchlistPhoneNumber = "07700900001"
+  val safePhoneNumber      = "07700900002"
+
+  val invalidPayload          = "{}"
+  val invalidInsightsEndpoint = s"$baseUrl/check/invalid-endpoint"
 
   override def beforeEach(): Unit =
-    deleteData()
+    clearWatchlistData()
 
-  override def afterAll(): Unit = {
-    deleteData()
+  override def afterEach(): Unit = {
+    clearWatchlistData()
     super.afterAll()
   }
 
-  val response: Any = getWatchlistPhoneNumbers
+  Feature("[PNI-1]- Phone Number Insights - Check if a phone number exists/does not exist on the watchlist") {
+    Scenario("[PNI.1.1] - Phone number exists on the watchlist") {
+      Given("the watchlist is empty")
+      assert(getWatchlistData.isEmpty)
 
-  Feature("[PNI-1] - Test the watchlist test-only endpoint") {
-    Scenario("Check if numbers exist on the phone number watchlist") {
-      Given("the watchlist is empty and the test data has been created")
-      assert(getWatchlistPhoneNumbers.isEmpty)
-      createData(10)
+      When(s"I add the phone number '$watchlistPhoneNumber' to the watchlist")
+      createWatchlistData(0, watchlistPhoneNumber)
 
-      When("the phone numbers are retrieved from the watchlist")
-      val phoneNumbers = getWatchlistPhoneNumbers
-      assert(phoneNumbers.nonEmpty)
+      And("I send a POST request to the check/insights endpoint")
+      postCheckInsightsRequest(watchlistPhoneNumber)
 
-      And("the watchlist contains the expected number of phone numbers")
-      assert(phoneNumbers.size == 14)
+      Then("the response should indicate that the number exists on the watchlist and the payload is correct")
+      assertPhoneNumberIsOnWatchlist(watchlistPhoneNumber)
+    }
+    Scenario("[PNI.1.2] - Phone number does not exist on the watchlist") {
+      Given("the watchlist is empty")
+      assert(getWatchlistData.isEmpty)
 
-      Then("the expected phone numbers should exist on the watchlist")
-      val presentNumbers = Seq("44798761728", "447928394728", "447927384756", "07783947887")
-      presentNumbers.foreach { number =>
-        assert(isPhoneNumberOnWatchlist(number))
-      }
+      When(s"I add the phone number '$watchlistPhoneNumber' to the watchlist")
+      createWatchlistData(0, watchlistPhoneNumber)
+
+      And("I send a POST request to the check/insights endpoint")
+      postCheckInsightsRequest(safePhoneNumber)
+
+      Then(s"the response should indicate that the number does not exist on the watchlist and the payload is correct")
+      assertPhoneNumberIsNotOnWatchlist(safePhoneNumber)
+    }
+  }
+
+  Feature("[PNI-2]- Phone Number Insights - POST Invalid requests to check/insights endpoint") {
+    Scenario("[PNI.2.1] - POST to check/insights with invalid payload and return a 400 HTTP response") {
+      Given("the watchlist is empty")
+      assert(getWatchlistData.isEmpty)
+
+      When("a POST to check/insights endpoint with an invalid payload")
+      val response = postInvalidPayloadRequest(invalidPayload)
+
+      Then("a 400 HTTP response is returned")
+      assert(response.status == 400)
+      assert(response.body.contains("\"message\":\"Invalid InsightsRequest payload"))
+    }
+    Scenario("[PNI.2.2] - POST to check/insights endpoint with an invalid endpoint and return a 404 HTTP response") {
+      Given("the watchlist is empty")
+      assert(getWatchlistData.isEmpty)
+
+      When("a POST request is sent via an invalid endpoint")
+      val response = postInvalidEndpoint(invalidInsightsEndpoint)
+
+      Then("a 404 HTTP response is returned")
+      assert(response.status == 404)
+      assert(response.body.contains("URI not found"))
+    }
+    Scenario("[PNI.2.3] - POST to check/insights with invalid credentials and return a 403 HTTP response") {
+      Given("the watchlist is empty")
+      assert(getWatchlistData.isEmpty)
+
+      When("a POST request is sent using invalid credentials")
+      val response = postInvalidAuthRequest(watchlistPhoneNumber)
+
+      Then("a 403 HTTP response is returned")
+      assert(response.status == 403)
+      assert(response.body.contains("USER_NOT_ALLOWED"))
+      assert(response.body.contains("Invalid credentials"))
     }
   }
 }
