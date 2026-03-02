1db86b29d28969ae445be06340f1c2c002dce72c
Ellie Newell <ellie.newell@digital.hmrc.gov.uk>
2026-03-02 08:50:01 +0000

    [CIR-2694] - Add graph data endpoints and update BaseSpec for graph data handling (#8)

--- src/test/scala/uk/gov/hmrc/api/specs/BaseSpec.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/BaseSpec.scala
@@ -28,11 +28,14 @@ import scala.concurrent.duration.DurationInt
 
 trait BaseSpec extends AnyFeatureSpec with GivenWhenThen with Matchers with HttpClientHelper {
 
-  val testOnlyEndpoint           = s"$testOnlyBaseUrl/test-only/watchlist/data"
-  val testOnlyEndpointDeleteData = s"$testOnlyEndpoint/delete"
-  val testOnlyEndpointCreateData = s"$testOnlyEndpoint/create"
-  val testOnlyEndpointCounts     = s"$testOnlyEndpoint/counts"
-  val checkInsightsEndpoint      = s"$baseUrl/check/insights"
+  val watchlistTestOnlyEndpoint  = s"$testOnlyBaseUrl/test-only/watchlist/data"
+  val testOnlyEndpointDeleteData = s"$watchlistTestOnlyEndpoint/delete"
+  val testOnlyEndpointCreateData = s"$watchlistTestOnlyEndpoint/create"
+  val testOnlyEndpointCounts     = s"$watchlistTestOnlyEndpoint/counts"
+
+  val graphDataTestOnlyEndpoint = s"$graphDatabaseUrl/test-only/cip-risk/str/vertex-data"
+
+  val checkInsightsEndpoint = s"$baseUrl/check/insights"
 
   def createWatchlistData(numberOfGeneratedPhoneNumbers: Int, manualPhoneNumbers: String): Unit = {
     val request =
@@ -57,7 +60,7 @@ trait BaseSpec extends AnyFeatureSpec with GivenWhenThen with Matchers with Http
 
   def getWatchlistData: Seq[String] = {
     val response = Await.result(
-      get(testOnlyEndpoint),
+      get(watchlistTestOnlyEndpoint),
       10.seconds
     )
     val body     = if (response.status == 200 && response.body.trim.nonEmpty) response.body else "{}"
@@ -69,7 +72,7 @@ trait BaseSpec extends AnyFeatureSpec with GivenWhenThen with Matchers with Http
 
   def getWatchlistDataCount: Int = {
     val response = Await.result(
-      get(testOnlyEndpoint),
+      get(watchlistTestOnlyEndpoint),
       10.seconds
     )
     val body     = if (response.status == 200 && response.body.trim.nonEmpty) response.body else "{}"
@@ -82,6 +85,50 @@ trait BaseSpec extends AnyFeatureSpec with GivenWhenThen with Matchers with Http
       Await.result(delete(testOnlyEndpointDeleteData), 10.seconds)
     val responseBody          = clearDataFromEndpoint.body
     responseBody should include regex "Deleted \\d+ watchlist phone numbers"
+  }
+
+  def createGraphData(numberOfEntries: Int, phoneNumber: String): Unit = {
+    val request =
+      s"""{
+         |  "randomEntriesToGenerate": $numberOfEntries,
+         |  "batchInsertSize": 101,
+         |  "vertexRecords": [{
+         |    "vertexId": 1,
+         |    "attributeId": "$phoneNumber",
+         |    "data": "{}",
+         |    "vertexType": "phone_number",
+         |    "hopsToClosestRisky": 2
+         |  }]
+         |}""".stripMargin
+
+    val createPhoneNumberInsightsTestOnlyData: StandaloneWSResponse =
+      Await.result(
+        post(graphDataTestOnlyEndpoint, request),
+        10.seconds
+      )
+
+    val responseBody = createPhoneNumberInsightsTestOnlyData.body
+    responseBody should include regex "Generated \\d+ vertices"
+    assert(createPhoneNumberInsightsTestOnlyData.status == 200)
+  }
+
+  def getGraphData: Seq[String] = {
+    val response = Await.result(
+      get(graphDataTestOnlyEndpoint),
+      10.seconds
+    )
+    val body     = if (response.status == 200 && response.body.trim.nonEmpty) response.body else "{}"
+    val json     = Json.parse(body)
+    (json \\ "phoneNumbersOnWatchlistEntries").headOption
+      .flatMap(_.asOpt[Seq[String]])
+      .getOrElse(Seq.empty)
+  }
+
+  def clearGraphData(): Assertion = {
+    val clearDataFromEndpoint =
+      Await.result(delete(graphDataTestOnlyEndpoint), 10.seconds)
+    val responseBody          = clearDataFromEndpoint.body
+    responseBody should include regex "Deleted \\d+ vertices"
   }
 
   def postCheckInsightsRequest(phoneNumber: String): StandaloneWSResponse = {
@@ -107,7 +154,7 @@ trait BaseSpec extends AnyFeatureSpec with GivenWhenThen with Matchers with Http
     response
   }
 
-  def assertPhoneNumberIsOnWatchlist(phoneNumber: String): Unit = {
+  def validateRiskyNumberPayload(phoneNumber: String): Unit = {
     val response = postCheckInsightsRequest(phoneNumber)
     val body     = response.body
     val json     = Json.parse(body)
@@ -116,19 +163,30 @@ trait BaseSpec extends AnyFeatureSpec with GivenWhenThen with Matchers with Http
     assert((json \ "insights" \ "risk" \ "score").asOpt[Int].contains(100))
     assert((json \ "insights" \ "risk" \ "reason").asOpt[String].contains("ON_WATCH_LIST"))
     assert((json \ "insights" \ "watchlistData" \ "isOnWatchlist").asOpt[Boolean].contains(true))
-    assert((json \ "insights" \ "graphData" \ "reasons").asOpt[Seq[String]].exists(_.contains("No risk")))
+    val reasons  = (json \ "insights" \ "graphData" \ "reasons").asOpt[Seq[String]].getOrElse(Seq.empty)
+    assert(reasons.nonEmpty)
+    assert(reasons.exists(_.contains(s"PHONE_NUMBER '$phoneNumber'")))
+    assert(reasons.exists(_.contains("hops from something risky")))
+
+    assert((json \ "insights" \ "graphData" \ "hops").asOpt[Int].contains(2))
+    assert((json \ "insights" \ "graphData" \ "avgHops").asOpt[BigDecimal].exists(_ >= 2))
   }
 
-  def assertPhoneNumberIsNotOnWatchlist(phoneNumber: String): Unit = {
+  def validateSafeNumberPayload(phoneNumber: String): Unit = {
     val response = postCheckInsightsRequest(phoneNumber)
     val body     = response.body
     val json     = Json.parse(body)
     assert((json \ "attributeType").asOpt[String].contains("PHONE_NUMBER"))
     assert((json \ "attributeValue").asOpt[String].contains(phoneNumber))
     assert((json \ "insights" \ "risk" \ "score").asOpt[Int].contains(0))
-    assert((json \ "insights" \ "risk" \ "reason").asOpt[String].contains("NOT_ON_WATCH_LIST. Reasons: No risk"))
+    assert((json \ "insights" \ "risk" \ "reason").asOpt[String].exists(_.startsWith("NOT_ON_WATCH_LIST")))
     assert((json \ "insights" \ "watchlistData" \ "isOnWatchlist").asOpt[Boolean].contains(false))
-    assert((json \ "insights" \ "graphData" \ "reasons").asOpt[Seq[String]].exists(_.contains("No risk")))
+    val reasons  = (json \ "insights" \ "graphData" \ "reasons").asOpt[Seq[String]].getOrElse(Seq.empty)
+    assert(reasons.nonEmpty)
+    assert(reasons.exists(_.contains(s"PHONE_NUMBER '$phoneNumber' is not in the database.")))
+    assert(reasons.exists(_.contains("hops from something risky")))
+
+    assert((json \ "insights" \ "graphData" \ "avgHops").asOpt[BigDecimal].exists(_ >= 2))
   }
 
   def postInvalidEndpoint(invalidEndpoint: String): StandaloneWSResponse = {
