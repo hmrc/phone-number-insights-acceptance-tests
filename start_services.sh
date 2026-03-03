2a740fd55cb79b99a372033a09fa4655924a298b
ellie-l-newell <113107044+ellie-l-newell@users.noreply.github.com>
2026-03-03 09:55:41 +0000

    [CIR-2694-b] - Updated config to fix failures in starting up services

--- start_services.sh
+++ start_services.sh
@@ -2,17 +2,22 @@
 
 sm2 --start PHONE_NUMBER_INSIGHTS_PROXY PHONE_NUMBER_INSIGHTS PHONE_NUMBER_GATEWAY DATASTREAM CIP_RISK INTERNAL_AUTH --appendArgs '{
         "PHONE_NUMBER_INSIGHTS_PROXY": [
-            "-J-Dauditing.enabled=true",
-            "-J-Dmicroservice.services.access-control.enabled=true",
-            "-J-Dmicroservice.services.access-control.allow-list.0=phone-number-gateway",
-            "-J-Dmicroservice.services.access-control.allow-list.1=phone-number-insights-acceptance-tests"
+            "-Dauditing.consumer.baseUri.port=6001",
+            "-Dauditing.consumer.baseUri.host=localhost",
+            "-Dauditing.enabled=false",
+            "-Dmicroservice.services.access-control.enabled=true",
+            "-Dmicroservice.services.access-control.allow-list.0=phone-number-gateway",
+            "-Dmicroservice.services.access-control.allow-list.1=phone-number-insights-acceptance-tests"
         ],
         "PHONE_NUMBER_INSIGHTS": [
-            "-J-Dapplication.router=testOnlyDoNotUseInAppConf.Routes",
-            "-J-Ddb.phonenumberinsights.url=jdbc:postgresql://localhost:5432/",
-            "-J-Dauditing.enabled=true"
+            "-Dapplication.router=testOnlyDoNotUseInAppConf.Routes",
+            "-Ddb.phonenumberinsights.url=jdbc:postgresql://localhost:5432/",
+            "-Dplay.evolutions.db.phonenumberinsights.autoApplyDowns=true",
+            "-Dauditing.enabled=false"
         ],
         "CIP_RISK": [
-            "-J-Dauditing.enabled=true"
+            "-Dapplication.router=testOnlyDoNotUseInAppConf.Routes",
+            "-Dplay.evolutions.db.risk.autoApplyDowns=true",
+            "-Dauditing.enabled=false"
         ]
     }'
